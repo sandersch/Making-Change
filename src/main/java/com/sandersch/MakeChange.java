b5be172e5b60fe8555d09fd77fa3fb005cca75c3
Charlie Sanders <sanderscharlie@gmail.com>
2011-12-02 21:55:18 -0600

    Greedy implementation of change making algorithm

--- src/main/java/com/sandersch/MakeChange.java
+++ src/main/java/com/sandersch/MakeChange.java
@@ -1,5 +1,7 @@
 package com.sandersch;
 
+import java.util.*;
+
 /**
  * Hello world!
  *
@@ -9,5 +11,22 @@ public class MakeChange
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+    }
+
+    public static List<Integer> change( int amount, List<Integer> coins )
+    {
+        Collections.sort( coins );
+        Collections.reverse( coins );
+
+        List<Integer> results = new ArrayList<Integer>();
+
+        for( Integer coin : coins )
+        {
+            int count = amount / coin;
+            amount %= coin;
+            for( int i = 0 ; i < count ; i++ ) { results.add( coin ); }
+        }
+
+        return results;
     }
 }
