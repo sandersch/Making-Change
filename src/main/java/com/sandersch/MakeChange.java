556e7284509cc2db2b11418c6ca7589f36eb4e31
Charlie Sanders <sanderscharlie@gmail.com>
2011-12-04 12:38:55 -0600

    Annonation and Clean Up for Dynamic Solution
    Adds comments and documentation to the dynamic programming solution for
    making change. Also does some basic clean up of the code and highlights
    places that need more testing

--- src/main/java/com/sandersch/MakeChange.java
+++ src/main/java/com/sandersch/MakeChange.java
@@ -25,58 +25,83 @@ public class MakeChange
         return results;
     }
 
+    /**
+     * Dynamic programming solution for change making.
+     *
+     * @param amount    The amount of change we need to find
+     * @param coins     A sorted list of coins to use when forming the solution  
+     *
+     * @return          The coins that form the minimal solution for amount
+     **/
     public static List<Integer> dynamic( int amount, List<Integer> coins )
     {
+        // the root of our solutions tree is the zero coin solution
+        // for making change for amount = 0
         Node root = new Node( null, 0, 0, 0 );
+        // n-th entry in solutions array holds the leaf node of optimal 
+        // solution for amount = n. 
         Node[] solutions = new Node[amount+1];
         solutions[0] = root;
-        for( int index = 1 ; index <= amount ; index++ )
-        {
-            solutions[index] = null;
-        }
+        // This queue holds a worklist of partial solutions that we use to 
+        // build larger solutions. We seed the list with the zero coin
+        // solution and build out solutions from there
         Queue<Node> queue = new LinkedList<Node>(); 
         queue.add( root );
 
+        // We process a queue of partial solutions and add a single coin
+        // to each solution to build larger partial solutions that are added
+        // to the queue until we find our desired solution for amount
         while( !queue.isEmpty() )
         {
             Node node = queue.remove();
+            // Add coins to existing solution to find larger optimal solutions.
+            // Start with the largest coin we haven't already ruled out
             for( int index = node.startingCoin ; index < coins.size() ; index++ )
             {
                 int coin = coins.get(index);
                 int newTotal = node.total + coin;  
         
                 // Prune solutions that aren't useful since they are bigger
-                // than our target or that we have already solved
+                // than our target amount or that we have already solved
                 if( (newTotal > amount) || (solutions[newTotal] != null) )
                 {
                     continue;
                 }
-               
+              
+                // This must me an optimal solution we haven't seen yet, so
+                // add it to the list of solutions
                 Node newNode = new Node( node, coin, newTotal, index );
                 solutions[newTotal] = newNode;
                 
                 // Stop now if we've found our answer!
                 if( newTotal == amount ) { break; }
-                
+               
+                // Otherwise add the new solution to the worklist and continue
+                // finding solutions
                 queue.add( newNode );
             }
         }
 
         // if we couldn't find a solution, return null
+        // XXX: is there something better to return here?
+        // TODO: test case for unsolveable change
         if( null == solutions[amount] )
         {
             return null;
         }
 
-        // Otherwise, unroll the solution and return that to the caller
+        // Otherwise, walk the solutions tree to find our full solution
         List<Integer> results = new ArrayList<Integer>();
         Node cursor = solutions[amount];
+        // XXX: what about solution for amount == 0?
         while( 0 != cursor.coin )
         {
             results.add( cursor.coin );
             cursor = cursor.parent;
         }
-        
+       
+        // reverse the order of the results for we are in descending order
+        // like the list of coins we used for this solution
         Collections.reverse( results );
         return results;
 
