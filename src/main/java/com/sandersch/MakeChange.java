552fca5e2912d49e68f08ecbca0b20550f8dc8bc
Charlie Sanders <sanderscharlie@gmail.com>
2011-12-04 20:18:57 -0600

    Clean Up and Reorg
    Adds a few new lines for clarity and moves the change() method to
    the top of the class. Minor clean up of code.

--- src/main/java/com/sandersch/MakeChange.java
+++ src/main/java/com/sandersch/MakeChange.java
@@ -9,6 +9,22 @@ import java.util.*;
 public class MakeChange 
 {
     /**
+     * Given an amount and some coins, find the minimal arrangement of
+     * coins whose total equals amount
+     *
+     * @param amount    The amount of change that we need to find
+     * @param coins     The coins we should use to find a solution
+     *
+     * @return          A list containing the minimal coins that total amount
+     **/
+    public static List<Integer> change( int amount, List<Integer> coins )
+    {
+        Collections.sort( coins, Collections.reverseOrder());
+        List<Integer> result = dynamic( amount, coins );
+        return result;
+    }
+
+    /**
      * Dynamic programming solution for change making.
      *
      * @param amount    The amount of change we need to find
@@ -21,28 +37,32 @@ public class MakeChange
         // the root of our solutions tree is the zero coin solution
         // for making change for amount = 0
         Node root = new Node( null, 0, 0, 0 );
+
         // n-th entry in solutions array holds the leaf node of optimal 
         // solution for amount = n. 
         Map<Integer,Node> solutions = new HashMap<Integer,Node>();
         solutions.put( 0, root );
+
         // This queue holds a worklist of partial solutions that we use to 
         // build larger solutions. We seed the list with the zero coin
         // solution and build out solutions from there
         Queue<Node> queue = new LinkedList<Node>(); 
         queue.add( root );
 
-        // We process a queue of partial solutions and add a single coin
+        // Process a queue of partial solutions and add a single coin
         // to each solution to build larger partial solutions that are added
         // to the queue until we find our desired solution
         while( !queue.isEmpty() && !solutions.containsKey( amount ) )
         {
+            // Start with existing solution off the queue
             Node node = queue.remove();
-            // Add coins to existing solution to find larger optimal solutions.
+          
+            // Add coins to existing solution to find larger solutions.
             // Start with the largest coin we haven't already ruled out
             for( int index = node.startingCoin ; index < coins.size() ; index++ )
             {
                 int coin = coins.get(index);
-                int newTotal = node.total + coin;  
+                int newTotal = node.total + coin; // new solution 
 
                 // Prune solutions that aren't useful since they are bigger
                 // than our target amount or that we have already solved
@@ -78,13 +98,14 @@ public class MakeChange
         // Otherwise, walk the solutions tree to find our full solution
         List<Integer> results = expandSolution( solutions.get( amount ));
        
-        // reverse the order of the results for we are in descending order
-        // like the list of coins we used for this solution
+        // reverse the order of the results to be in descending value
+        // like the list of coins we started with
         Collections.reverse( results );
 
         return results;
     }
 
+    // helper method that extracts the full solution when given the leaf node`
     private static List<Integer> expandSolution( Node leaf )
     {
         List<Integer> results = new ArrayList<Integer>();
@@ -95,13 +116,7 @@ public class MakeChange
         return results;
     }
 
-    public static List<Integer> change( int amount, List<Integer> coins )
-    {
-        Collections.sort( coins, Collections.reverseOrder());
-        List<Integer> result = dynamic( amount, coins );
-        return result;
-    }
-
+    // internal data structure used by algorithm to store solutions
     static class Node
     {
         public Node parent;
